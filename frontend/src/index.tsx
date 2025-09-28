88f513bd1299e9668758856f41a716b1b7f5ee9a
cybrd <brysuralta@gmail.com>
2025-09-28 23:17:48 +0800

    setting up 2

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -10,12 +10,10 @@ import { NotProtected, Protected } from "./components/protected";
 
 import "./index.scss";
 
-const root = document.getElementById("root") as HTMLElement;
+const root = document.getElementById("root");
 
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
-  );
+if (!(root instanceof HTMLElement)) {
+  throw new Error("Root element not found");
 }
 
 render(
