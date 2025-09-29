bde109840c4bd28ead7ce3ea4743a0463c181214
cybrd <brysuralta@gmail.com>
2025-09-29 14:59:57 +0800

    backend setting up 6

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,15 @@
 import type { APIGatewayEvent, Context } from "aws-lambda";
 import createAPI, { Request, Response } from "lambda-api";
+import { sum } from "./services/sum";
 
 const api = createAPI();
 
-api.get("/status", (_req: Request, _res: Response) => {
-  return { status: "ok" };
+api.get("/", (_req: Request, res: Response) => {
+  res.status(500);
+});
+
+api.get("/sum", (_req: Request, res: Response) => {
+  res.send(sum(1, 2));
 });
 
 export const handler = (event: APIGatewayEvent, context: Context) => {
